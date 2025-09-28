f0abfcf8dd72c0bd03f6570e62f15bc06a59da20
SyedArfan04 <1221103586@student.mmu.edu.my>
2025-09-28 22:48:23 +0800

    Added Final_Application folder with packaged build

---
+++ Final_Application/.qt/QtDeployTargets.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_SmartStreamingPlatform_FILE C:/Users/G15 AMD Power/Documents/degree sem3 y2/Digi Comp/SmartStreamingPlatform/build/Desktop_Qt_6_9_2_MinGW_64_bit-Debug/SmartStreamingPlatform.exe)
+set(__QT_DEPLOY_TARGET_SmartStreamingPlatform_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_SmartStreamingPlatform_RUNTIME_DLLS C:/Qt/6.9.2/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.9.2/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.9.2/mingw_64/bin/Qt6Core.dll)
